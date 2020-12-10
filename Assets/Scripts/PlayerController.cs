31cff78d61316309ef87cdb892e5523deb49ab3b
Danny Sternbeck <danny.sternbeck@gmail.com>
2020-12-10 10:52:29 +0100

    Game copied into new repository

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float movementSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+    public int countToWin;
+    public int foodCount;
+    
+    
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+        
+        
+    }
+
+    void ProcessInput()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized; 
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * movementSpeed, moveDirection.y * movementSpeed);  
+    }
+
+    private void OnTriggerEnter2D (Collider2D collision){
+        if(collision.CompareTag("Food")){
+            print("I FOUND SOME FOOD!");
+            Destroy(collision.gameObject);
+            foodCount += 1;
+            
+            if(foodCount == countToWin){
+                Debug.Log("I GOT ALL OF THE FOOD!");
+                gameManager.Win();
+                
+        
+                }
+
+            }
+
+
+        }
+
+    }
+
